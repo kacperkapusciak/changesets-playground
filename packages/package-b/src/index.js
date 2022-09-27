5d6c3ee2f7abcd66f6c2551c6a6d41cb6d903209
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-09-27 11:54:25 +0200

    fix: add more logging

--- packages/package-b/src/index.js
+++ packages/package-b/src/index.js
@@ -3,5 +3,6 @@
 const myIsOdd = require('@kacperkapusciak/package-a');
 
 module.exports = function myIsEven(value) {
+  console.log('value: %d, isEven: %d', value, !myIsOdd(value));
   return !myIsOdd(value);
 }

a104f850dea92419f7d9b513ec80c620d6e1d876
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-09-27 11:20:21 +0200

    chore: add example

---
+++ example/src/index.js
@@ -0,0 +1,9 @@
+const myIsOdd = require('@kacperkapusciak/package-a');
+const myIsEven = require('@kacperkapusciak/package-b');
+
+
+const value = 5;
+
+console.log('isOdd: ', myIsOdd(value));
+
+console.log('isEven: ', myIsEven(value));

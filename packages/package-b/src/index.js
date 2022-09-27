fea3fdb0240f1caca5c5843ca45cd1ba1fd0d1b2
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-09-27 16:42:27 +0200

    fix: changes

--- packages/package-b/src/index.js
+++ packages/package-b/src/index.js
@@ -3,6 +3,5 @@
 const myIsOdd = require('@kacperkapusciak/package-a');
 
 module.exports = function myIsEven(value) {
-  console.log('value: %d, isEven: %d', value, !myIsOdd(value));
   return !myIsOdd(value);
 }

d24e23e6d8621548e1a3f9836c9877b06773576f
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2022-09-27 14:41:00 +0200

    fix: add logging again (#13)

---
+++ .changeset/fluffy-mice-impress.md
@@ -0,0 +1,5 @@
+---
+"@kacperkapusciak/package-a": minor
+---
+
+Add logging to package-a
--- packages/package-a/src/index.js
+++ packages/package-a/src/index.js
@@ -3,5 +3,6 @@
 const isOdd = require('is-odd');
 
 module.exports = function myIsOdd(value) {
+  console.log('value: %d, isOdd: %d', value, isOdd(value));
   return isOdd(value);
 };

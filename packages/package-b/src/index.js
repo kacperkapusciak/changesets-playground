fbc7b0e6b5dd00f308bdbc305133a184d3d37762
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-09-30 13:36:19 +0200

    fix: logger

--- packages/package-b/src/index.js
+++ packages/package-b/src/index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const myIsOdd = require('@kacperkapusciak/package-a');
-const logger = require('@kacperkapusciak/package-c');
 
 module.exports = function myIsEven(value) {
   // when in development mode, log the value
   if (process.env.NODE_ENV === 'development') {
+    const logger = require('@kacperkapusciak/package-c');
     // important fix
     logger.log(value);
   }
